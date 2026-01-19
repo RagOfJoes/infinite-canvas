8db3315511e200aff3ff851d4bd91786b56f5d10
RagOfJoes <dev@ragofjoes.dev>
2026-01-19 14:03:39 -0800

    feat: add createContext helper function

---
+++ src/lib/create-context.ts
@@ -0,0 +1,45 @@
+import { createContext as createSolidContext, useContext as useSolidContext } from "solid-js";
+import type { ContextProviderComponent, Context as SolidContext } from "solid-js";
+
+export type CreateContextArgs<T> = {
+	defaultValue?: T | undefined;
+	hookName?: string | undefined;
+	errorMessage?: string | undefined;
+	name: string;
+	providerName?: string | undefined;
+	strict?: boolean | undefined;
+};
+
+export type CreateContextReturn<T> = [ContextProviderComponent<T>, () => T, SolidContext<T>];
+
+export function createContext<T>(args: CreateContextArgs<T>): CreateContextReturn<T> {
+	const {
+		defaultValue,
+		strict = true,
+		hookName = "useContext",
+		name,
+		providerName = "Provider",
+		errorMessage,
+	} = args;
+
+	const Context = createSolidContext(defaultValue, { name });
+
+	const useContext = () => {
+		const context = useSolidContext(Context);
+
+		if (!context && strict) {
+			const error = new Error(
+				errorMessage ??
+					`${hookName} returned \`undefined\`. Must wrap component within ${providerName}`,
+			);
+
+			error.name = "ContextError";
+
+			throw error;
+		}
+
+		return context;
+	};
+
+	return [Context.Provider, useContext, Context] as CreateContextReturn<T>;
+}
